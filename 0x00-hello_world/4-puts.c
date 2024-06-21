6daf6233d2db3c38aa8142f676413595c293c15f
Damsscene <damsscene008@gmail.com>
2024-06-20 19:46:16 -0700

    prints hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /**
  * main - entry which prints hello world
- * return is 0 which means success
+ * Return: is 0 which means success
  */
 
-int main(void) {
+int main(void)
+{
 
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
