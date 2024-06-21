456c1d54958e936cef4871151b83458c9f5eec64
Damsscene <damsscene008@gmail.com>
2024-06-20 19:43:11 -0700

    prints hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-int main(void){
+
+/**
+ * main - entry which prints hello world
+ * return is 0 which means success
+ */
+
+int main(void) {
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
