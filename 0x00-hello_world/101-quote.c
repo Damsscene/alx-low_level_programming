b929e9a4a1a70f3af3c82ee5a92f8917ff820730
Damsscene <damsscene008@gmail.com>
2024-06-21 03:17:03 -0700

    prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, 59);
-return 1;
+const char *m= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, m, 59);
+return (1);
 }
