7d14e0b4f271e47586edd9d26a14e29f6236450f
Damsscene <damsscene008@gmail.com>
2024-06-21 03:06:21 -0700

    prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the specified message to standard error
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, 59);
+return 1;
+}
