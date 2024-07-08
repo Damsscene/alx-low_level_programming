eeca0bed775a4fd5c8e555e93e8df40a542f4102
Damsscene <damsscene008@gmail.com>
2024-07-08 07:33:43 -0700

    outputs putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * This function prints "_putchar" followed by a newline character
+ *
  * main - Entry point of the program
  *
  * Return: 0 to indicate successful execution
