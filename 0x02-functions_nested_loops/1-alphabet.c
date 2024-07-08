b6809236fba343016d7ae54142cf2b2d75ef86b8
Damsscene <damsscene008@gmail.com>
2024-07-08 08:19:58 -0700

    initial commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,7 +9,7 @@ void print_alphabet(void)
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
 	int i = 0;
 
-	while (alphabet[i] != '\0') 
+	while (alphabet[i] != '\0')
 	{
 		_putchar(alphabet[i]);
 		i++;
