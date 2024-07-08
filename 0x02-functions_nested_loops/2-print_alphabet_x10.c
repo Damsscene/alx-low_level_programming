9e68ba549fdd9e815e95a942bee6cad4830d5850
Damsscene <damsscene008@gmail.com>
2024-07-08 09:01:45 -0700

    initial commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,26 +1,25 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet in lowercase followed by a new line
+ * print_alphabet_x10 - prints the alphabet in lowercase followed by a new line
  * Return: 0
  */
 void print_alphabet(void)
 {
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
-    int i = 0;
-    
-    while (alphabet[i] != '\0')
-    {
-	    _putchar(alphabet[i]);
-	    i++;
-    }
+	int i = 0;
+	while (alphabet[i] != '\0')
+	{
+		_putchar(alphabet[i]);
+		i++;
+	}
 }
 
 void print_alphabet_x10(void)
 {
 	int j;
 	for (j = 0; j < 10; j++)
-    {
-	    print_alphabet();
-    }
+	{
+		print_alphabet();
+	}
 }
