baef1f0416985cdb6098830fa208c7df446fdef6
Damsscene <damsscene008@gmail.com>
2024-07-08 07:59:49 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lowercase alphabet
+ * Return: 0
+ */
+
+void print_alphabet(void) 
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i = 0;
+	while (alphabet[i] != '\0') {
+		_putchar(alphabet[i]);
+		i++;
+	}
+}
