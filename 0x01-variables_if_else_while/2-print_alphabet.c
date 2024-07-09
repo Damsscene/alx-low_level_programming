eb00b3a49b4e4cfaf0c65aab9730d43dd09206c6
Damsscene <damsscene008@gmail.com>
2024-07-09 12:17:42 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - for lower
+ * return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
