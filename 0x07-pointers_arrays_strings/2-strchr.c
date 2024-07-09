55640db0cef90c1dc662a843189c21cdef29e1dc
Damsscene <damsscene008@gmail.com>
2024-07-09 03:06:37 -0700

    initial commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,20 +3,20 @@
 /**
  * _strchr - Locates a character in a string.
  * @s: Pointer to the string to be searched.
- * @e: The character to locate in the string.
+ * @c: The character to locate in the string.
  * Return: a pointer or NULL if the character is not found.
  */
-char *_strchr(char *s, char e)
+char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-		if (*s == e)
+		if (*s == c)
 		{
 			return (s);
 		}
 		s++;
 	}
-	if (e == '\0')
+	if (c == '\0')
 	{
 		return (s);
 	}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,9 +1,8 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, const char *src, unsigned int n);
-char *_strchr(char *s, char e);
+char *_strchr(char *s, char c);
 
 #endif /* MAIN_H_ */
