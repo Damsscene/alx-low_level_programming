802b27663bd8e902101763755d2a10b3a2303544
Damsscene <damsscene008@gmail.com>
2024-07-09 11:56:51 -0700

    initial commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strpbrk - locates the first occurrence
+ *
+ * return: 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		char *a = accept;
+
+		while (*a != '\0')
+		{
+			if (*s == *a)
+			{
+				return (s);
+			}
+			a++;
+		}
+		s++;
+	}
+	return (NULL);
+}
