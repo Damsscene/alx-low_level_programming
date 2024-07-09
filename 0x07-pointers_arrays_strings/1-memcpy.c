adc2d77c984f329fa6f9fce5fabfc1aa9b827c59
Damsscene <damsscene008@gmail.com>
2024-07-09 02:29:13 -0700

    initial commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies memory area.
+ * @dest: Pointer to the destination memory area where bytes are copied to.
+ * @src: Pointer to the source memory area where bytes are copied from.
+ * @n: Number of bytes to copy.
+ *
+ * Return: a pointer to the destination memory area dest.
+ */
+
+char *_memcpy(char *dest, const char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (unsigned int i = 0; i < n; ++i)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, const char *src, unsigned int n);
 
 #endif /* MAIN_H_ */
