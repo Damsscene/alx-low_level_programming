c12a45db710c8691eda3371d551bbfe0a98430f0
Damsscene <damsscene008@gmail.com>
2024-07-08 23:08:22 -0700

    commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,7 @@
  * add - adds two integers and returns the result
  * @a: the first integer to add
  * @b: the second integer to add
- * 
+ *
  * Return: the result of the addition
  */
 int add(int a, int b)
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,17 +21,18 @@ void print_to_98(int n)
 				printf("%d, ", n);
 			}
 		}
-		else
+	}
+	else
+	{
+		for (; n >= 98; n--)
 		{
-			for (; n >= 98; n--)
+			if (n == 98)
 			{
-				if (n == 98)
-				{
-					printf("%d\n", n);
-				}
-				else
-				{printf("%d, ", n);
-				}
+				printf("%d\n", n);
+			}
+			else
+			{
+				printf("%d, ", n);
 			}
 		}
 	}
